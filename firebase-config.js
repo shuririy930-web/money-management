3b01508e679136adba836b204077c08784460d6e
shuririy930-web <shuririy930@icloud.com>
2026-04-23 18:40:50 +0900

    firebase-config.js を作成

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// 本番用
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTI1uXYabHYz1H4XtUv5lJ2xOEwkGQVAo",
+  authDomain: "money-management-255cf.firebaseapp.com",
+  projectId: "money-management-255cf",
+  storageBucket: "money-management-255cf.firebasestorage.app",
+  messagingSenderId: "458774945741",
+  appId: "1:458774945741:web:6cd4dee1f2811b313b6e84"
+};
